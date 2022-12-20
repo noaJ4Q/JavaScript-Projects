f0d505945d6b511a6f11beb1e44685f509ce1cc0
noe <noe.jaraq@gmail.com>
2022-12-20 18:37:14 -0500

    calculator styles ready

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,3 +1,25 @@
-let result = document.getElementById('result').textContent;
-result = Number(result);
+
+function clear(){
+    document.getElementById('clear-control').onclick = function(){
+        document.getElementById('result').innerHTML = 0;
+    }
+}
+
+function getNumber(){
+    return document.getElementById('result');
+}
+
+function numbersButton(){
+
+    let numeroInput = document.getElementsByClassName('number-controls');
+    let numero = getNumber();
+
+    if(numero != '0'){
+        document.getElementById('result').innerHTML = numero+numeroInput;
+    }
+
+}
+
+function getResult(){
+}
 
